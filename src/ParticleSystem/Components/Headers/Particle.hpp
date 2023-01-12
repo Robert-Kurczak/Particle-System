5e283a2ad87b5578a2a93e7438df932e17de9bb6
Robert Kurczak <323303@uwr.edu.pl>
2023-01-12 16:15:47 +0100

    Particle implementation

--- src/ParticleSystem/Components/Headers/Particle.hpp
+++ src/ParticleSystem/Components/Headers/Particle.hpp
@@ -4,17 +4,16 @@
 
 class Particle{
 	public:
-		ofVec3f position = {0, 0, 0};
-		ofVec3f velocity = {0, 0, 0};
-		float radius = 1;
-		float lifeTime = 10;
-		float mass = 1;
-		ofColor color = {255, 255, 255};
+		ofVec3f position;
+		ofVec3f velocity;
+		float radius;
+		float lifeTime;
+		float mass;
+		ofColor color;
 
-		bool alive = true;
+		bool alive;
 
-		void draw(){
-			ofSetColor(color);
-			ofDrawSphere(position, radius);
-		}
+		Particle();
+
+		void draw();
 };
