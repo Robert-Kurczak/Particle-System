f95120b2e06e5febf0ea515e364e514b85d0e568
Robert Kurczak <323303@uwr.edu.pl>
2023-01-12 16:32:11 +0100

    Updater implementation

--- src/ParticleSystem/Components/Headers/Updater.hpp
+++ src/ParticleSystem/Components/Headers/Updater.hpp
@@ -13,57 +13,30 @@ class GravityUpdater: public Updater{
 		float acceleration;
 
 	public:
-		GravityUpdater(float acceleration = 9.81): acceleration(acceleration){}
+		GravityUpdater(float acceleration = 9.81);
 
-		void update(float deltaTime, Particle& particle) override{
-			particle.velocity.y += acceleration * deltaTime;
-			particle.position.y += particle.velocity.y * deltaTime;
-		}
+		void update(float deltaTime, Particle& particle) override;
 };
 
 class LifetimeUpdater: public Updater{
 	public:
-		void update(float deltaTime, Particle& particle) override{
-			particle.lifeTime -= deltaTime;
-
-			if(particle.lifeTime <= 0){
-				particle.alive = false;
-			}
-		}
+		void update(float deltaTime, Particle& particle) override;
 };
 
 class FloorCollisionUpdater: public Updater{
 	public:
-		float floorPosition = 0;
+		float floorPosition;
 
-		FloorCollisionUpdater(){}
-		FloorCollisionUpdater(float floorPosition): floorPosition(floorPosition){}
+		FloorCollisionUpdater(float floorPosition = 0);
 
-		void update(float deltaTime, Particle& particle) override{
-			if(particle.position.y + particle.radius > floorPosition){
-				particle.position.y = floorPosition - particle.radius;
-				particle.velocity = ofVec3f(0, 0, 0);
-			}
-		}
+		void update(float deltaTime, Particle& particle) override;
 };
 
 class SphereCollisionUpdater: public Updater{
 	private:
 		std::vector<ofSpherePrimitive>& spheres; 
 	public:
-		SphereCollisionUpdater(std::vector<ofSpherePrimitive>& spheres): spheres(spheres){}
+		SphereCollisionUpdater(std::vector<ofSpherePrimitive>& spheres);
 
-		void update(float deltaTime, Particle& particle) override{
-			for(size_t i = 0; i < spheres.size(); i++){
-				float distance = ofVec3f(spheres[i].getPosition()).distance(particle.position);
-				float sphereRadius = spheres[i].getRadius();
-
-				if(distance < sphereRadius + particle.radius){
-					float overlap = (sphereRadius + particle.radius) - distance;
-
-					particle.position -= overlap * particle.velocity.getNormalized();
-					particle.velocity = ofVec3f(0, 0, 0);
-				}
-			}
-		};
+		void update(float deltaTime, Particle& particle) override;
 };
--- src/ParticleSystem/Components/Updater.cpp
+++ src/ParticleSystem/Components/Updater.cpp
@@ -1,1 +1,56 @@
 #include "Headers/Updater.hpp"
+
+//---Gravity Updater---
+GravityUpdater::GravityUpdater(float acceleration):
+acceleration(acceleration)
+{}
+
+void GravityUpdater::update(float deltaTime, Particle& particle){
+	particle.velocity.y += acceleration * deltaTime;
+	particle.position.y += particle.velocity.y * deltaTime;
+}
+//------
+
+//---Lifetime Updater---
+void LifetimeUpdater::update(float deltaTime, Particle& particle){
+	particle.lifeTime -= deltaTime;
+
+	if(particle.lifeTime <= 0){
+		particle.alive = false;
+	}
+}
+//------
+
+//---Floor Collision Updater---
+FloorCollisionUpdater::FloorCollisionUpdater(float floorPosition):
+floorPosition(floorPosition)
+{}
+
+
+void FloorCollisionUpdater::update(float deltaTime, Particle& particle){
+	if(particle.position.y + particle.radius > floorPosition){
+		particle.position.y = floorPosition - particle.radius;
+		particle.velocity = ofVec3f(0, 0, 0);
+	}
+}
+//------
+
+//---Sphere Collision Updater---
+SphereCollisionUpdater::SphereCollisionUpdater(std::vector<ofSpherePrimitive>& spheres):
+spheres(spheres)
+{}
+
+void SphereCollisionUpdater::update(float deltaTime, Particle& particle){
+	for(size_t i = 0; i < spheres.size(); i++){
+		float distance = ofVec3f(spheres[i].getPosition()).distance(particle.position);
+		float sphereRadius = spheres[i].getRadius();
+
+		if(distance < sphereRadius + particle.radius){
+			float overlap = (sphereRadius + particle.radius) - distance;
+
+			particle.position -= overlap * particle.velocity.getNormalized();
+			particle.velocity = ofVec3f(0, 0, 0);
+		}
+	}
+}
+//------
